5f8d223dbf999799ee465e2d44130eb6cf35a0fe
Brendt Bly <brendt.bly@outlook.com>
2018-04-18 16:13:00 -0500

    rendering simple pug file

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 // require all dependencies
 var express = require('express');
 var app = express();
+
+// set up the template engine
+app.engine('pug', require('pug').__express)
+app.set('views', './views');
+app.set('view engine', 'pug');
  
 // Routes
 app.get('/', function (req, res) {
  
     // render the 'index' template
-    res.send('hello');
- 
+    //res.send('hello');
+    res.render('index');
 });
  
 // start up the server
@@ -15,3 +20,5 @@ app.listen(3000, function () {
     console.log('Listening on http://localhost:3000');
 });
  
+
+//made with help from https://courses.ideate.cmu.edu/48-739/s2017/?page_id=177
